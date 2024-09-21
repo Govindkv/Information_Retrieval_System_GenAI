cf162b33c74144cc4de340f0b32d52c9546509db
Govindkv <Govind26663355@gmail.com>
2024-09-21 15:16:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Govindkv',
+    author_email= 'Govind26663355@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
